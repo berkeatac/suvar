969d81b8f23e30e4ba0f88aa48b2c920ca01e225
berkeatac <berkeatac@sabanciuniv.edu>
2020-04-03 14:33:17 +0300

    init suvar

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useContext, useEffect } from "react";
+import Card from "../elements/Card/Card";
+
+const App = () => {
+  return (
+    <div>
+      <Card>
+        <p style={{}}>hello</p>
+        <p style={{}}>hello</p>
+        <p style={{}}>hello</p>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,46 @@
+const images = [
+  {
+    id: 1,
+    src: "_yasinaydin.JPG",
+    title: "foo",
+    creator: "Yasin Aydin",
+    grade: "ungraded"
+  },
+  {
+    id: 2,
+    src: "aCross_yasinaydin.JPG",
+    title: "aCross",
+    creator: "Yasin Aydin",
+    grade: "ungraded"
+  },
+  {
+    id: 3,
+    src: "final2_yasinaydin.JPG",
+    title: "Final 2",
+    creator: "Yasin Aydin",
+    grade: "ungraded"
+  },
+  {
+    id: 4,
+    src: "final_yasinaydin.JPG",
+    title: "Final",
+    creator: "Yasin Aydin",
+    grade: "ungraded"
+  },
+  {
+    id: 5,
+    src: "Last Day2_yasinaydin.JPG",
+    title: "Last Day 2",
+    creator: "Yasin Aydin",
+    grade: "ungraded"
+  },
+  {
+    id: 6,
+    src: `Last Day_yasinaydin.JPG`,
+    title: "Last Day",
+    creator: "Yasin Aydin",
+    grade: "ungraded"
+  }
+];
+
+export default images;
